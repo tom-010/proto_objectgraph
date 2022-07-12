de343b9d7b334e72dd083329639fd5f665b87669
Thomas Deniffel <tdeniffel@gmail.com>
2022-07-12 18:27:56 +0200

    model encoded

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	pb "deniffel.com/examples/objectgraph/pkg/pb"
 	"github.com/gogo/protobuf/jsonpb"
@@ -15,16 +15,30 @@ func main() {
 		FirstName: "Thomas",
 		LastName:  "Deniffel",
 	}
-	fmt.Println(&employee)
+	log.Println(&employee)
 	patient := pb.Patient{
 		Id:         "3458021",
 		Salutation: "Herr",
 		LastName:   "Mustermann",
 		Employee:   &pb.Employee_Ref{Id: "123", Loaded: &employee},
 	}
-	fmt.Println(&patient)
+	log.Println(&patient)
+
+	employee2 := pb.Employee{
+		Id:        "4920393",
+		FirstName: "Christoph",
+		LastName:  "Wölfle",
+		Patients:  &pb.Patient_Many{All: []*pb.Patient{&patient}},
+	}
+
+	if employee.Patients != nil {
+		log.Printf("the employee, has %d patients", len(employee.Patients.All))
+	}
+	if employee2.Patients != nil {
+		log.Printf("the employee2, has %d patients", len(employee2.Patients.All))
+	}
 
 	result, _ := m.MarshalToString(&patient)
-	fmt.Println(result)
+	log.Println(result)
 
 }
